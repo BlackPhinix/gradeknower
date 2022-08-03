c5727f13597f31fb98c915a9c1b1f7254d105bb1
Abena <72131115+BlackPhinix@users.noreply.github.com>
2022-08-03 11:50:55 +0300

    Add files via upload

---
+++ gradeknower.py
@@ -0,0 +1,21 @@
+grade = int(input("Enter your grade: "))
+if grade < 40:
+    print ("Your grade is F")
+elif grade <= 45:
+    print ("your grade is D")
+elif grade <= 59:
+    print ("your grade is C")
+elif grade <= 64:
+    print ("your grade is C+")
+elif grade <= 70:
+    print ("your grade is B-")
+elif grade <= 75:
+    print ("your grade is B")
+elif grade <= 78:
+    print ("your grade is B+")
+elif grade <= 80:
+    print ("your grade is A-")
+elif grade <= 100:
+    print ("your grade is A")
+else:
+    start = False
